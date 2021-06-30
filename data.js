f7364c6c2fe31949d40bdece72cc20922e4c7a48
Hiro Mataba <aganzemataba@gmail.com>
2021-06-30 15:09:16 +0200

    Add data in data.js

---
+++ data.js
@@ -0,0 +1,48 @@
+const data = {
+  features: [
+    {
+      key: 0,
+      featureImage: './img/b1.jpeg',
+      featureName: 'Business',
+      featureDescription: ' "They manage the knowledge transfer really well."',
+      featureMessage: ' Because they build a relationship that is based on trust . Over they years they have proven that delivery is fast and stable',
+    },
+    {
+      key: 1,
+      featureImage: './img/b2.jpeg',
+      featureName: 'Business',
+      featureDescription: '"They manage the knowledge transfer really well."',
+      featureMessage: ' Because they build a relationship that is based on trust . Over they years they have proven that delivery is fast and stable',
+    },
+    {
+      key: 2,
+      featureImage: './img/b7.jpeg',
+      featureName: 'Business',
+      featureDescription: '"They manage the knowledge transfer really well."',
+      featureMessage: ' Because they build a relationship that is based on trust . Over they years they have proven that delivery is fast and stable',
+    },
+    {
+      key: 3,
+      featureImage: './img/b1.jpeg',
+      featureName: 'Business',
+      featureDescription: '"They manage the knowledge transfer really well."',
+      featureMessage: ' Because they build a relationship that is based on trust . Over they years they have proven that delivery is fast and stable',
+    },
+    {
+      key: 4,
+      featureImage: './img/b4.jpeg',
+      featureName: 'Business',
+      featureDescription: '"They manage the knowledge transfer really well."',
+      featureMessage: ' Because they build a relationship that is based on trust . Over they years they have proven that delivery is fast and stable',
+    },
+    {
+      key: 3,
+      featureImage: './img/b7.jpeg',
+      featureName: 'Business',
+      featureDescription: '"They manage the knowledge transfer really well."',
+      featureMessage: ' Because they build a relationship that is based on trust . Over they years they have proven that delivery is fast and stable',
+    },
+  ],
+};
+
+export default data;
--- index.js
+++ index.js
@@ -13,3 +13,44 @@ closeMenuBtn.addEventListener('click', () => {
   menuLinks.style.display = 'none';
   menuLinks.classList.remove('menu-links');
 });
+const speakerGrid = document.querySelector('.feature .speaker-grid');
+function implementFeatures() {
+  myFeatures.forEach((feature) => {
+    // Create Article
+    const featureArticle = document.createElement('article');
+    featureArticle.classList.add('feature-article');
+    const featureImage = document.createElement('div');
+    featureImage.classList.add('feature-image');
+    const featurePic = document.createElement('img');
+    featurePic.src = feature.featureImage;
+    feature.alt = 'Feature Image';
+    featureImage.appendChild(featurePic);
+    featureArticle.appendChild(featureImage);
+
+    // Add Article Info
+    const articleInfo = document.createElement('div');
+    articleInfo.classList.add('arcticle-info');
+    const authorName = document.createElement('h3');
+    authorName.classList.add('author-name');
+    authorName.innerText = feature.featureName;
+    articleInfo.appendChild(authorName);
+
+    // Add Article Description
+    const authorWord = document.createElement('p');
+    authorWord.classList.add('author-word');
+    authorWord.innerText = feature.featureDescription;
+    articleInfo.appendChild(authorWord);
+
+    // Add Article Message
+    const articleMessage = document.createElement('p');
+    articleMessage.classList.add('author-message');
+    articleMessage.innerText = feature.featureMessage;
+    articleInfo.appendChild(articleMessage);
+
+    featureArticle.appendChild(articleInfo);
+
+    speakerGrid.appendChild(featureArticle);
+  });
+}
+
+implementFeatures();
